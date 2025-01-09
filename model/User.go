914d6dff87bbe4734a3cb4ae27c95b9ad6727cd0
aojiaodemeng <845113602@qq.com>
2025-01-09 15:51:46 +0800

    feat: 使用内置的scrypt对密码进行安全加密

--- model/User.go
+++ model/User.go
@@ -2,7 +2,10 @@ package model
 
 import (
 	"cant_forget/utils/status_code"
+	"encoding/base64"
+	"golang.org/x/crypto/scrypt"
 	"gorm.io/gorm"
+	"log"
 	"time"
 )
 
@@ -29,9 +32,24 @@ func CheckUserExist(name string) (code int) {
 
 // 新增用户
 func CreateUser(data *User) int {
+	data.Password = ScryptPw(data.Password)
 	err := db.Create(&data).Error
 	if err != nil {
 		return status_code.ERROR
 	}
 	return status_code.SUCCESS
+}
+
+// 密码加密
+func ScryptPw(password string) string {
+	const KeyLen = 10
+	salt := make([]byte, 8)
+	salt = []byte{12, 32, 4, 6, 66, 22, 222, 11}
+
+	HashPw, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, KeyLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fpw := base64.StdEncoding.EncodeToString(HashPw)
+	return fpw
 }
